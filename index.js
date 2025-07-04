d2ae9fdf5ab6801731f635bd406af4ac9a903c8c
jose-victorio <j.victorio2028@gmail.com>
2025-07-04 16:05:27 -0300

    melhorias
    melhorias do projeto

--- index.js
+++ index.js
@@ -7,25 +7,28 @@ botao.addEventListener("click", (evento) => {
     evento.preventDefault()
         if(input.value === ""){
             alert("Nada escrito.")
+            target
         }
             
-            const itemDaLista = document.createElement("li")
-            const containerDaLista = document.createElement("div")
-            containerDaLista.classList.add("lista-item-container") //acessar a classe
-            const inputCheckbox = document.createElement("input")
-            inputCheckbox.type = "checkbox" //deficiniu o input
-            inputCheckbox.id = "checkbox-" + contador++ //criou o "id que muda" com o checkbox + a variavel contador
-            const itemInserido = document.createElement("p")
-            itemInserido.innerText = input.value 
+        const itemDaLista = document.createElement("li")
+        const containerDaLista = document.createElement("div")
+        containerDaLista.classList.add("lista-item-container")
+        const inputcheckbox = document.createElement("input")
+        inputcheckbox.type = "checkbox"
+        inputcheckbox.id = "checkbox-" + contador++
+        const itemInserido = document.createElement("p")
+        itemInserido.innerText = input.value
+        
+        
+        containerDaLista.appendChild(inputcheckbox)
+        containerDaLista.append(itemInserido)
 
-            containerDaLista.appendChild(inputCheckbox)//adicionando filhos ao containerDaLista
-            containerDaLista.appendChild(itemInserido)
+        itemDaLista.appendChild(containerDaLista)
+        listaDeItens.appendChild(itemDaLista)
 
-            itemDaLista.appendChild(containerDaLista)
-
-            listaDeItens.appendChild(itemDaLista)
-
+        
 })
+
 
 
 
